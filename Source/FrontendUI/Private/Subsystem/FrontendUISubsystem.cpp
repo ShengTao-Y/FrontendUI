89b9686cc8e59f249a15797f21b28b24267a2244
DESKTOP-OPTJ0CI\Administrator <2772435978@qq.com>
2025-08-06 20:19:35 +0800

    feat：【p9】创建FrontendUISubsystem子系统

---
+++ Source/FrontendUI/Private/Subsystem/FrontendUISubsystem.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystem/FrontendUISubsystem.h"
+
+UFrontendUISubsystem* UFrontendUISubsystem::Get(const UObject* WorldContextObject)
+{
+	if(GEngine)
+	{
+		UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject,EGetWorldErrorMode::Assert);
+		return UGameInstance::GetSubsystem<UFrontendUISubsystem>(World->GetGameInstance());
+	}
+	return nullptr;
+}
+
+bool UFrontendUISubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	if(!CastChecked<UGameInstance>(Outer)->IsDedicatedServerInstance()) //这个类的实例不在专属服务器上
+	{
+		TArray<UClass*> FoundClasses;
+		GetDerivedClasses(GetClass(),FoundClasses);//返回指定父类的子类的数组
+
+		return FoundClasses.IsEmpty();//当有不同的子类继承自这个类时，父类将不会被创建
+	}
+	return false;
+}
+
+void UFrontendUISubsystem::RegisterCreatedPrimaryLayoutWidget(UWidget_PrimaryLayout* InCreateWidget)
+{
+	check(InCreateWidget);
+	
+	CreatedPrimaryLayout = InCreateWidget;
+}
---
+++ Source/FrontendUI/Public/Subsystem/FrontendUISubsystem.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "FrontendUISubsystem.generated.h"
+
+class UWidget_PrimaryLayout;
+
+UCLASS()
+class FRONTENDUI_API UFrontendUISubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	static UFrontendUISubsystem* Get(const UObject* WorldContextObject);
+
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+
+	UFUNCTION(BlueprintCallable)
+	void RegisterCreatedPrimaryLayoutWidget(UWidget_PrimaryLayout* InCreateWidget);
+
+private:
+
+	UPROPERTY(Transient)
+	UWidget_PrimaryLayout* CreatedPrimaryLayout;
+	
+};
