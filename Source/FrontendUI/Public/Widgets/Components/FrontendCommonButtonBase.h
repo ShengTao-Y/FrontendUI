1408eb5085d92d6feeb2597d673198a95e927a00
DESKTOP-OPTJ0CI\Administrator <2772435978@qq.com>
2025-08-11 16:33:37 +0800

    feat：【p3】增加Button基类

---
+++ Source/FrontendUI/Public/Widgets/Components/FrontendCommonButtonBase.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "FrontendCommonButtonBase.generated.h"
+
+class UCommonTextBlock;
+
+UCLASS(Abstract,BlueprintType,meta = (DisableNaiveTick))
+class FRONTENDUI_API UFrontendCommonButtonBase : public UCommonButtonBase
+{
+	GENERATED_BODY()
+
+public:
+	void SetButtonText(FText InText);
+
+private:
+	virtual void NativePreConstruct() override;
+
+	//**** Bound Widgets ****//
+	UPROPERTY(meta = (BindWidgetOptional))
+	UCommonTextBlock* CommonTextBlock_ButtonText;
+	//**** Bound Widgets ****//
+
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Frontend Button",meta = (AllowPrivateAccess = "true"))
+	FText ButtonDisplayText;
+
+	//文本是否使用大写
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Frontend Button",meta = (AllowPrivateAccess = "true"))
+	bool bUserUpperCaseForButtonText = false;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Frontend Button",meta = (AllowPrivateAccess = "true"))
+	FText ButtonDescriptionText;
+	
+};
