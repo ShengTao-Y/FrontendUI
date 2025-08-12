83fc600cf341f518feca1b9d3da9814fcdcf9ec5
DESKTOP-OPTJ0CI\Administrator <2772435978@qq.com>
2025-08-12 15:46:58 +0800

    Notice：【p14】增加创建三种界面的静态函数

--- Source/FrontendUI/Private/Widgets/Widget_ConfirmScreen.cpp
+++ Source/FrontendUI/Private/Widgets/Widget_ConfirmScreen.cpp
@@ -3,3 +3,57 @@
 
 #include "Widgets/Widget_ConfirmScreen.h"
 
+UConfirmScreenInfoObject* UConfirmScreenInfoObject::CreateOKScreen(const FText& InScreenTitle,
+	const FText& InScreenMessage)
+{
+	UConfirmScreenInfoObject* InfoObject = NewObject<UConfirmScreenInfoObject>();
+	InfoObject->ScreenTitle = InScreenTitle;
+	InfoObject->ScreenMessage = InScreenMessage;
+
+	FConfirmScreenButtonInfo OKButtonInfo;
+	OKButtonInfo.ConfirmScreenButtonType = EConfirmScreenButtonType::Close;
+	OKButtonInfo.ButtonTextToDisplay = FText::FromString(TEXT("OK"));
+
+	InfoObject->AvailableScreenButtons.Add(OKButtonInfo);
+	return InfoObject;
+}
+
+UConfirmScreenInfoObject* UConfirmScreenInfoObject::CreateYesNoScreen(const FText& InScreenTitle,
+	const FText& InScreenMessage)
+{
+	UConfirmScreenInfoObject* InfoObject = NewObject<UConfirmScreenInfoObject>();
+	InfoObject->ScreenTitle = InScreenTitle;
+	InfoObject->ScreenMessage = InScreenMessage;
+
+	FConfirmScreenButtonInfo YesButtonInfo;
+	YesButtonInfo.ConfirmScreenButtonType = EConfirmScreenButtonType::Confirmed;
+	YesButtonInfo.ButtonTextToDisplay = FText::FromString(TEXT("Yes"));
+
+	FConfirmScreenButtonInfo NoButtonInfo;
+	NoButtonInfo.ConfirmScreenButtonType = EConfirmScreenButtonType::Cancelled;
+	NoButtonInfo.ButtonTextToDisplay = FText::FromString(TEXT("No"));
+
+	InfoObject->AvailableScreenButtons.Add(YesButtonInfo);
+	InfoObject->AvailableScreenButtons.Add(NoButtonInfo);
+	return InfoObject;
+}
+
+UConfirmScreenInfoObject* UConfirmScreenInfoObject::CreateOKCancelScreen(const FText& InScreenTitle,
+	const FText& InScreenMessage)
+{
+	UConfirmScreenInfoObject* InfoObject = NewObject<UConfirmScreenInfoObject>();
+	InfoObject->ScreenTitle = InScreenTitle;
+	InfoObject->ScreenMessage = InScreenMessage;
+
+	FConfirmScreenButtonInfo OKButtonInfo;
+	OKButtonInfo.ConfirmScreenButtonType = EConfirmScreenButtonType::Confirmed;
+	OKButtonInfo.ButtonTextToDisplay = FText::FromString(TEXT("OK"));
+
+	FConfirmScreenButtonInfo CancelButtonInfo;
+	CancelButtonInfo.ConfirmScreenButtonType = EConfirmScreenButtonType::Cancelled;
+	CancelButtonInfo.ButtonTextToDisplay = FText::FromString(TEXT("Cancel"));
+
+	InfoObject->AvailableScreenButtons.Add(OKButtonInfo);
+	InfoObject->AvailableScreenButtons.Add(CancelButtonInfo);
+	return InfoObject;
+}
--- Source/FrontendUI/Public/Widgets/Widget_ConfirmScreen.h
+++ Source/FrontendUI/Public/Widgets/Widget_ConfirmScreen.h
@@ -3,8 +3,47 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "FrontendTypes/FrontendEnumTypes.h"
 #include "Widgets/Widget_ActivatableBase.h"
 #include "Widget_ConfirmScreen.generated.h"
+
+USTRUCT(BlueprintType)
+struct FConfirmScreenButtonInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	EConfirmScreenButtonType ConfirmScreenButtonType = EConfirmScreenButtonType::Unknown;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	FText ButtonTextToDisplay;
+};
+
+UCLASS(Abstract,BlueprintType,meta = (DisableNaiveTick))
+class FRONTENDUI_API UConfirmScreenInfoObject : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	//用来创建"OK"的UI界面
+	static UConfirmScreenInfoObject* CreateOKScreen(const FText& InScreenTitle,const FText& InScreenMessage);
+	//用来创建"Yes/No"的UI界面
+	static UConfirmScreenInfoObject* CreateYesNoScreen(const FText& InScreenTitle,const FText& InScreenMessage);
+	//用来创建"OK/Cancel"的UI界面
+	static UConfirmScreenInfoObject* CreateOKCancelScreen(const FText& InScreenTitle,const FText& InScreenMessage);
+
+	
+	UPROPERTY(Transient)
+	FText ScreenTitle;
+	
+	UPROPERTY(Transient)
+	FText ScreenMessage;
+
+	//根据这个数组就可以知道需要添加几个按钮
+	UPROPERTY(Transient)
+	TArray<FConfirmScreenButtonInfo> AvailableScreenButtons;
+	
+};
 
 class UCommonTextBlock;
 class UDynamicEntryBox;
