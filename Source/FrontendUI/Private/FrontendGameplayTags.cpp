27dbcdf3dae459009019644fd4464522d619bf2b
DESKTOP-OPTJ0CI\Administrator <2772435978@qq.com>
2025-08-14 15:53:46 +0800

    feat：【p2-Set Up Options Screen】增加“OptionScreen”的基类

--- Source/FrontendUI/Private/FrontendGameplayTags.cpp
+++ Source/FrontendUI/Private/FrontendGameplayTags.cpp
@@ -13,5 +13,7 @@ namespace FrontendGameplayTags
 	//前端控件
 	UE_DEFINE_GAMEPLAY_TAG(Frontend_Widget_PressAnyKeyScreen,"Frontend.Widget.PressAnyKeyScreen");
 	UE_DEFINE_GAMEPLAY_TAG(Frontend_Widget_MainMenuScreen,"Frontend.Widget.MainMenuScreen");
-	UE_DEFINE_GAMEPLAY_TAG(Frontend_Widget_ConfirmScreen,"Frontend.Widget.ConfirmScreen");
+	UE_DEFINE_GAMEPLAY_TAG(Frontend_Widget_ConfirmScreen,"Frontend.Widget.OptionsScreen");
+	
+	UE_DEFINE_GAMEPLAY_TAG(Frontend_Widget_OptionsScreen,"Frontend.Widget.ConfirmScreen");
 }
---
+++ Source/FrontendUI/Public/Widgets/Options/Widget_OptionScreen.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Widget_ActivatableBase.h"
+#include "Widget_OptionScreen.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract,BlueprintType,meta = (DisableNaiveTick))
+class FRONTENDUI_API UWidget_OptionScreen : public UWidget_ActivatableBase
+{
+	GENERATED_BODY()
+};
