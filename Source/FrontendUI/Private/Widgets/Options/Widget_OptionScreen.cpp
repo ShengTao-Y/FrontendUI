e210b447b298302ccb1131f035f32edafb5ba5e6
DESKTOP-OPTJ0CI\Administrator <2772435978@qq.com>
2025-08-14 16:07:52 +0800

    feat：【p3】C++中注册输入操作

--- Source/FrontendUI/Private/Widgets/Options/Widget_OptionScreen.cpp
+++ Source/FrontendUI/Private/Widgets/Options/Widget_OptionScreen.cpp
@@ -2,3 +2,37 @@
 
 
 #include "Widgets/Options/Widget_OptionScreen.h"
+#include "FrontentDebugHelper.h"
+#include "ICommonInputModule.h"
+#include "Input/CommonUIInputTypes.h"
+
+void UWidget_OptionScreen::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	if (!ResetAction.IsNull())
+	{
+		ResetActionHandle = RegisterUIActionBinding(
+			FBindUIActionArgs(
+				ResetAction,
+				true,
+				FSimpleDelegate::CreateUObject(this, &ThisClass::OnResetBoundActionTriggered)
+			)
+		);
+	}
+	RegisterUIActionBinding(
+		FBindUIActionArgs(
+			ICommonInputModule::GetSettings().GetDefaultBackAction(),
+			true,
+			FSimpleDelegate::CreateUObject(this,&ThisClass::OnBackBoundActionTriggered)
+		)
+	);
+	
+}
+void UWidget_OptionScreen::OnResetBoundActionTriggered()
+{
+	Debug::Print(TEXT("Reset bound action triggered"));
+}
+void UWidget_OptionScreen::OnBackBoundActionTriggered()
+{
+	DeactivateWidget();
+}
--- Source/FrontendUI/Public/Widgets/Options/Widget_OptionScreen.h
+++ Source/FrontendUI/Public/Widgets/Options/Widget_OptionScreen.h
@@ -13,4 +13,18 @@ UCLASS(Abstract,BlueprintType,meta = (DisableNaiveTick))
 class FRONTENDUI_API UWidget_OptionScreen : public UWidget_ActivatableBase
 {
 	GENERATED_BODY()
+
+protected:
+	//~ Begin UUserWidget Interface
+	virtual void NativeOnInitialized() override;
+	//~ End UUserWidget Interface
+private:	
+	void OnResetBoundActionTriggered();
+	void OnBackBoundActionTriggered();
+
+	//重置当前选项的输入
+	UPROPERTY(EditDefaultsOnly, Category = "Frontend Options Screen", meta = (RowType = "/Script/CommonUI.CommonInputActionDataBase"))
+	FDataTableRowHandle ResetAction;
+	//存储重置当前选项的输入句柄
+	FUIActionBindingHandle ResetActionHandle;
 };
