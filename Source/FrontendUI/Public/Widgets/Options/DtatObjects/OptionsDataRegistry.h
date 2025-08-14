72a95124907778f69f8384337aa19aa00c46b920
DESKTOP-OPTJ0CI\Administrator <2772435978@qq.com>
2025-08-14 17:36:58 +0800

    feat：【p9】增加UOptionsDataRegistry

---
+++ Source/FrontendUI/Public/Widgets/Options/DtatObjects/OptionsDataRegistry.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "OptionsDataRegistry.generated.h"
+
+class UListDataObject_Collection;
+/**
+ * 
+ */
+UCLASS()
+class FRONTENDUI_API UOptionsDataRegistry : public UObject
+{
+	GENERATED_BODY()
+
+	
+public:
+	//在UOptionsdatareregistry类型的对象创建之后，由options screen调用
+	void InitOptionsDataRegistry(ULocalPlayer* InOwningLocalPlayer);
+
+	const TArray<UListDataObject_Collection*>& GetRegisteredOptionsTabCollections() const { return RegisteredOptionsTabCollections;}
+private:
+	void InitGameplayCollectionTab();
+	void InitAudioCollectionTab();
+	void InitVideoCollectionTab();
+	void InitControlCollectionTab();
+
+	UPROPERTY(Transient)
+	TArray<UListDataObject_Collection*> RegisteredOptionsTabCollections;
+};
